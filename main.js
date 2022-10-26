625b0e8087b1f059fd1f9d704d75ff542dc4bdf3
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-26 15:16:27 +0200

    Esercizio 2 fatto con do while

--- main.js
+++ main.js
@@ -15,4 +15,18 @@
 // Esercizio 2
 
 let ciclo = 0;
+let sommaProvvisoria = 0;
 
+
+do{
+    let num = parseInt(prompt("inserisci numero"))
+
+    sommaProvvisoria += num;
+
+    console.log("Somma Provvisoria: ", sommaProvvisoria);
+
+    ciclo++
+
+} while (ciclo < 5)
+
+console.log("Somma Finale: ", sommaProvvisoria);
