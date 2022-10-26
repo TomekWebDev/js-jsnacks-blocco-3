34564f609e265361206852e1bb5f4fee79758b17
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-26 15:10:49 +0200

    Esercizio 1 finito

---
+++ main.js
@@ -0,0 +1,18 @@
+// // Esercizio 1
+
+// let arrayNumeri = [];
+
+// let somma = 0;
+
+// while(somma < 50){
+//     let num = parseInt(prompt("inserisci numero"));
+//     somma += num;
+// }
+
+// console.log(somma);
+
+
+// Esercizio 2
+
+let ciclo = 0;
+
