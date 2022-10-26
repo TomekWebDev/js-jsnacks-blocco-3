ab1e9122793c4c85c7fd18a15a8ae43b5a800116
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-26 16:27:11 +0200

    Esercizio 3 fatto - prova dare nomi != ad array

--- main.js
+++ main.js
@@ -6,27 +6,56 @@
 
 // while(somma < 50){
 //     let num = parseInt(prompt("inserisci numero"));
+//     arrayNumeri.push(num)
 //     somma += num;
 // }
 
 // console.log(somma);
+// console.log(arrayNumeri);
 
 
-// Esercizio 2
+// // Esercizio 2
 
-let ciclo = 0;
-let sommaProvvisoria = 0;
+// let ciclo = 0;
+// let sommaProvvisoria = 0;
 
 
-do{
-    let num = parseInt(prompt("inserisci numero"))
+// do{
+//     let num = parseInt(prompt("inserisci numero"))
 
-    sommaProvvisoria += num;
+//     sommaProvvisoria += num;
 
-    console.log("Somma Provvisoria: ", sommaProvvisoria);
+//     console.log("Somma Provvisoria: ", sommaProvvisoria);
 
-    ciclo++
+//     ciclo++
 
-} while (ciclo < 5)
+// } while (ciclo < 5)
 
-console.log("Somma Finale: ", sommaProvvisoria);
+// console.log("Somma Finale: ", sommaProvvisoria);
+
+
+// Esercizio 3
+
+let n = parseInt(prompt("Inserire numero di array da creare"))
+let i = 0;
+
+while(i < n){
+    
+    var array = new Array();
+
+    let k = 0;
+
+    while(k < 10){
+        array.push(Math.floor(Math.random() * 100));
+
+        k++
+    }
+
+    console.log(array);
+    
+    i++
+};
+
+
+
+
